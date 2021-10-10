a1cb477c742d2e75aefbd8b7f252aa0c6c38a025
Shashank S. Khasare <shashank.khasare@gmail.com>
2021-10-10 22:14:00 +0530

    Remove primary annotation.

--- src/main/java/com/example/demo/config/DataSourceConfiguration.java
+++ src/main/java/com/example/demo/config/DataSourceConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import com.example.demo.TenantAwareDataSource;
 import com.zaxxer.hikari.HikariDataSource;
@@ -15,14 +14,12 @@ import com.zaxxer.hikari.HikariDataSource;
 public class DataSourceConfiguration {
 
 	@Bean
-	@Primary
 	@ConfigurationProperties("multitenancy.tenant.datasource")
 	public DataSourceProperties tenantDataSourceProperties() {
 		return new DataSourceProperties();
 	}
 
 	@Bean
-	@Primary
 	@ConfigurationProperties("multitenancy.tenant.datasource.hikari")
 	public DataSource tenantDataSource() {
 		HikariDataSource dataSource = tenantDataSourceProperties().initializeDataSourceBuilder()
